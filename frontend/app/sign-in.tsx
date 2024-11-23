0a77affe28db889b99cc35d9c04fee24d88d5491
fatimabanu12 <al20760360@ite.edu.mx>
2024-11-22 17:15:27 -0800

    inicio de sesion diseño

--- frontend/app/sign-in.tsx
+++ frontend/app/sign-in.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import { useSession } from "./ctx";
 import { router } from "expo-router";
-import { Button, TextInput, View, Alert } from "react-native";
+import {
+  Button,
+  TextInput,
+  View,
+  Alert,
+  Text,
+  StyleSheet,
+  Image,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function Signin() {
   const { signIn } = useSession();
@@ -18,13 +29,81 @@ export default function Signin() {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={styles.contenedor_image}>
+        <Image
+          style={styles.image_background}
+          source={require("../assets/images/Vector.png")}
+        />
+        <Image
+          source={require("../assets/images/aura.png")}
+          style={styles.image}
+        />
+      </View>
+
+      <Text style={styles.inicio_sesion}>Iniciar sesion</Text>
+      <Text style={styles.leyenda}>
+        Introduce tu numero de telefono con el que te haz registrado y te
+        enviaremos un codigo para iniciar sesion.
+      </Text>
+      <Text style={styles.subtitle}> Numero telefonico*</Text>
       <TextInput
-        placeholder="username"
+        placeholder="*"
         value={username}
         onChangeText={setUsername}
         style={{ borderWidth: 1, padding: 10, marginBottom: 20, width: "80%" }}
       />
-      <Button title="Sign in" onPress={handleSignIn} />
+
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Ingresar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  subtitle: {
+    width: "80%",
+    margin: 10,
+  },
+  contenedor_image: {
+    width: "100%",
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    marginBottom: "20%",
+  },
+  image_background: {
+    width: "100%",
+    marginTop: -270,
+  },
+  image: {
+    position: "absolute",
+    height: "55%",
+    width: "70%",
+    marginTop: -150,
+  },
+  inicio_sesion: {
+    fontFamily: "roboto",
+    fontWeight: "bold",
+    fontSize: 30,
+    marginTop: -150,
+    margin: 10,
+  },
+  leyenda: {
+    fontSize: 15,
+    textAlign: "center",
+    margin: 10,
+  },
+  button: {
+    backgroundColor: "#61678B",
+    width: "80%",
+    marginBottom: 10,
+    alignItems: "center",
+    borderRadius: 10,
+    paddingVertical: 10,
+  },
+  buttonText: {
+    color: "white",
+  },
+});
